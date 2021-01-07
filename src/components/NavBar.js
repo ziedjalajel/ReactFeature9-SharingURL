c9bf3f285bfbebe5e8ed102192a740ad241e06da
Lailz <lailaabdulraheem@gmail.com>
2021-01-07 11:56:42 +0300

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+// Styling
+import { ThemeButton } from "../styles";
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <h4 className="navbar-brand">add your logo</h4>
+      <div className="navbar-nav ml-auto">
+        <a className="nav-item">Products</a>
+        <ThemeButton className="nav-item" onClick={props.toggleTheme}>
+          {props.currentTheme === "light" ? "Dark" : "Light"} Mode
+        </ThemeButton>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
